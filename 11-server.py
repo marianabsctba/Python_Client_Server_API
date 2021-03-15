718367288c0509ea088eb84f7186062b41bfe4f2
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2021-03-15 12:19:57 -0300

    Create 11-server.py

---
+++ 11-server.py
@@ -0,0 +1,35 @@
+import socket, os
+socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostname()
+port = 8881
+
+socket_server.bind((host, port))
+socket_server.listen()
+
+print("Servidor de nome", host, "esperando conexão na porta", port)
+
+while True:
+    (socket_client,addr) = socket_server.accept() # espera indefinidamente
+    print("Conectado a:", str(addr))
+    
+    msg = socket_client.recv(2048)
+    name = msg.decode('ascii')
+    
+    if os.path.isfile(name):
+        l = os.stat(name).st_size
+        socket_client.send(str(l).encode('ascii'))
+        
+        fil = open(name, 'rb')
+        bytes = fil.read(4096)
+        
+        while bytes:
+            socket_client.send(bytes)
+            bytes = fil.read(4096)
+
+        fil.close()
+    else:
+        print("Arquivo não encontrado...")
+        socket_client.send('-1'.encode('ascii'))
+        socket_client.close()
+
+socket_servidor.close()
