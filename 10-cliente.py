1a809935a25f5d1abe5dac08971160e00cbee720
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2021-03-15 14:06:03 -0300

    Update 10-cliente.py

--- 10-cliente.py
+++ 10-cliente.py
@@ -1,7 +1,5 @@
 import socket, sys, os, time
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 name = input('Qual o nome do arquivo com extensao? ')
 
 
@@ -13,29 +11,34 @@ def print_status(bytes, tam):
     text = text + 'de ' + '{:<.2f}'.format(tam_bytes) + ' KB'
     print(text)
 
-try:
-    s.connect((socket.gethostname(), 8881))
-    s.send(name.encode('ascii'))
-    msg = s.recv(12)
-    lenght = int(msg.decode('ascii'))
-    if lenght >= 0:
-        print('Arquivo encontrado... aguarde...')
-        time.sleep(3)
-        fil = open('download\\'+name, "wb")
-        summ = 0
-        bytes = s.recv(4096)
-        
-        while bytes:
-            fil.write(bytes)
-            summ = summ + len(bytes)
-            os.system('cls')
-            print_status(summ, lenght)
-            bytes = s.recv(4096)
-        fil.close()
-
-
-except Exception as erro:
-	print(str(erro))
 	
-s.close()
+def main():
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	try:
+	    s.connect((socket.gethostname(), 8881))
+	    s.send(name.encode('ascii'))
+	    msg = s.recv(12)
+	    lenght = int(msg.decode('ascii'))
+	    if lenght >= 0:
+		print('Arquivo encontrado... aguarde...')
+		time.sleep(3)
+		fil = open('download\\'+name, "wb")
+		summ = 0
+		bytes = s.recv(4096)
 
+		while bytes:
+		    fil.write(bytes)
+		    summ = summ + len(bytes)
+		    os.system('cls')
+		    print_status(summ, lenght)
+		    bytes = s.recv(4096)
+		fil.close()
+
+
+	except Exception as erro:
+		print(str(erro))
+
+	s.close()
+
+if __name__ == '__main__':
+    main()
