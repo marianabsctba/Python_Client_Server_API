3c92fda91252a5c6cc61c170d1fd6dc0515e402b
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2021-03-15 15:33:57 -0300

    Update 9-serverudp-versão_2.py

--- 9-serverudp-versão_2.py
+++ 9-serverudp-versão_2.py
@@ -1,6 +1,6 @@
-#SERVER UDP
 
-import socket
+# server udp versao 2
+
 import psutil
 
 udpServer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -14,19 +14,17 @@ utilizado = round(disco.used / (1024**3), 2)
 
 def serveru():
     print("Esperando cliente na porta", 9991)
-    while True: # espera indefinidamente
-        try:
-            (msg, client) = udpServer.recvfrom(1024)
-            if msg.decode() == 'y':
-                udpServer.sendto('\ntotal : {}GB\nlivre :{}GB\nutilizado: {}GB'.format(total, disponivel, utilizado).encode('utf-8'), client)
+    try:
+        (msg, client) = udpServer.recvfrom(1024)
+        if msg.decode() == 'y':
+            udpServer.sendto('\ntotal : {}GB\nlivre :{}GB\nutilizado: {}GB'.format(total, disponivel, utilizado).encode('utf-8'), client)
 
-            else:
-                udpServer.sendto('Erro: Digite (s).'.encode('utf-8'), client)
-
+        else:
+            udpServer.sendto('Erro: Digite (s).'.encode('utf-8'), client)
             udpServer.close()
-
-        except Exception as error:
-            print(error)
+    
+    except Exception as error:
+        print(error)
 
 
 if __name__ == '__main__':
