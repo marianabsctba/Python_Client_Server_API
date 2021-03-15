9e469313479929bf337dbc20a76beb881e59010e
Mariana Bührer Sukevicz <62853909+marianabsctba@users.noreply.github.com>
2021-03-15 12:18:19 -0300

    Create 10-cliente.py

---
+++ 10-cliente.py
@@ -0,0 +1,41 @@
+import socket, sys, os, time
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+name = input('Qual o nome do arquivo com extensao? ')
+
+
+def print_status(bytes, tam):
+    kbytes = bytes/1024
+    tam_bytes = tam/1024
+    text = 'Fazendo o download... '
+    text = text + '{:<.2f}'.format(kbytes) + ' KB '
+    text = text + 'de ' + '{:<.2f}'.format(tam_bytes) + ' KB'
+    print(text)
+
+try:
+    s.connect((socket.gethostname(), 8881))
+    s.send(name.encode('ascii'))
+    msg = s.recv(12)
+    lenght = int(msg.decode('ascii'))
+    if lenght >= 0:
+        print('Arquivo encontrado... aguarde...')
+        time.sleep(3)
+        fil = open('download\\'+name, "wb")
+        summ = 0
+        bytes = s.recv(4096)
+        
+        while bytes:
+            fil.write(bytes)
+            summ = summ + len(bytes)
+            os.system('cls')
+            print_status(summ, lenght)
+            bytes = s.recv(4096)
+        fil.close()
+
+
+except Exception as erro:
+	print(str(erro))
+	
+s.close()
+
